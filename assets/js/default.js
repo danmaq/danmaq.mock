0131983af47fa8d2427a899fb4003a8aee43f32e
Shuhei Nomura <info@danmaq.com>
2017-02-23 00:51:09 +0900

    スキルのランダム表示対応。

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,17 +1,56 @@
 'use strict;'
 
-$(window).ready(
-    function() {
-        var achieves = $('#achieve li');
+const achieves_category = [
+    'vm', 'iaas', 'saas', 'manage', 'backend', 'db',
+    'front', 'flash', 'os', 'linux', 'ide', 'gameapi',
+    'lang', 'misc', 'video', 'graphic'
+];
+
+const achieves_shared = [
+    'iaas', 'manage', 'flash', 'os',
+    'ide', 'misc', 'video', 'graphic'
+];
+
+const sort_rand = () => Math.random() - 0.5;
+const achieves_fixed =
+    achieves_category
+    .filter(v => !achieves_shared.includes(v));
+const achieves_selected =
+    achieves_shared
+    .sort(sort_rand)
+    .filter((v, i) => i < 4)
+    .concat(achieves_fixed)
+    .sort(sort_rand);
+
+const on_ready =
+    () => {
+        const select =
+            (v) => {
+                const acs = $('#achieve li[data-achieve="' + v + '"]');
+                const sum =
+                    acs
+                    .map(function() { return $(this).data('p'); })
+                    .get()
+                    .reduce((p, c) => p + parseInt(c));
+                let rnd = Math.round(Math.random() * sum);
+                const sub = v => (rnd -= parseInt(v)) <= 0;
+                const found =
+                    acs.filter(function() { return sub($(this).data('p')); });
+                let e = (found.length == 0 ? acs : found).first();
+                e.show();
+                e.addClass('achieve-' + v);
+                //e.text('');
+            };
         //achieves.each(console.log);
         //console.log('achieves: ', achieves.length);
         //console.log('achieves: ', achieves.data('achieve'));
-        //achieves.hide();
-    });
+        $('#achieve li').hide();
+        achieves_selected.forEach(select);
+    };
 
-$(window).scroll(
-    function() {
-        var nav = $('nav');
+const on_scroll =
+    () => {
+        let nav = $('nav');
         if ($(document).scrollTop() <= 64) {
             nav.addClass('navbar-expand');
             nav.addClass('navbar-dark');
@@ -21,4 +60,7 @@ $(window).scroll(
             nav.removeClass('navbar-dark');
             nav.addClass('navbar-light');
         }
-    });
+    };
+
+$(window).ready(on_ready);
+$(window).scroll(on_scroll);
