8539d6c5fd52a2038c50b7af6361e351c6a7ace0
Shuhei Nomura <info@danmq.com>
2017-02-22 21:27:42 +0900

    jQueryを用いた実験。

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,13 +1,24 @@
+'use strict;'
+
+$(window).ready(
+    function() {
+        var achieves = $('#achieve li');
+        achieves.each(console.log);
+        //console.log('achieves: ', achieves.length);
+        //console.log('achieves: ', achieves.data('achieve'));
+        //achieves.hide();
+    });
+
 $(window).scroll(
     function() {
-        $('img.logo-large');
+        var nav = $('nav');
         if ($(document).scrollTop() <= 64) {
-            $('nav').addClass('navbar-expand');
-            $('nav').addClass('navbar-dark');
-            $('nav').removeClass('navbar-light');
+            nav.addClass('navbar-expand');
+            nav.addClass('navbar-dark');
+            nav.removeClass('navbar-light');
         } else {
-            $('nav').removeClass('navbar-expand');
-            $('nav').removeClass('navbar-dark');
-            $('nav').addClass('navbar-light');
+            nav.removeClass('navbar-expand');
+            nav.removeClass('navbar-dark');
+            nav.addClass('navbar-light');
         }
     });
