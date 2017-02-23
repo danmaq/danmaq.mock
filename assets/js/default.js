8aab3f1eaef99ce690426fc6ce8c89d81b9006d2
Shuhei Nomura <info@danmq.com>
2017-02-23 14:13:20 +0900

    画像差し替え処理実装

--- assets/js/default.js
+++ assets/js/default.js
@@ -22,30 +22,56 @@ const achieves_selected =
     .concat(achieves_fixed)
     .sort(sort_rand);
 
+const select_subachieve =
+    v => {
+        const acs = $('#achieve li[data-achieve="' + v + '"]');
+        const sum =
+            acs
+            .map(function() { return $(this).data('p'); })
+            .get()
+            .reduce((p, c) => p + parseInt(c));
+        let rnd = Math.floor(Math.random() * sum);
+        const sub = v => (rnd -= parseInt(v)) <= 0;
+        const found =
+            acs.filter(function() { return sub($(this).data('p')); });
+        let e = (found.length == 0 ? acs : found).first();
+        return { k: v, v: e };
+    };
+
+const action_subview =
+    h => {
+        let e = h.v;
+        e.show();
+        e.removeClass('hidden');
+        const text = e.text();
+        if (e.data('img')) {
+            const burl = './assets/img/achieves/' + h.k + '/';
+            e.text('');
+            e.removeClass('achieve-text');
+            e.append(
+                '<img alt="' + text + '" title="' + text +
+                '" src="' + burl + e.data('img') +
+                '" width="80" height="80" />');
+        }
+        else if (e.data('i')) {
+            e.text('');
+            e.removeClass('achieve-text');
+            e.append(
+                '<i title="' + text + '" class="fa fa-' + e.data('i') +
+                '" aria-hidden="true"></i>');
+        }
+        else
+        {
+            e.addClass('achieve-' + h.k);
+        }
+    };
+
 const on_ready =
     () => {
-        const select =
-            (v) => {
-                const acs = $('#achieve li[data-achieve="' + v + '"]');
-                const sum =
-                    acs
-                    .map(function() { return $(this).data('p'); })
-                    .get()
-                    .reduce((p, c) => p + parseInt(c));
-                let rnd = Math.round(Math.random() * sum);
-                const sub = v => (rnd -= parseInt(v)) <= 0;
-                const found =
-                    acs.filter(function() { return sub($(this).data('p')); });
-                let e = (found.length == 0 ? acs : found).first();
-                e.addClass('achieve-' + v);
-                e.show();
-                e.removeClass('hidden');
-                if (e.data('img')) {
-                    console.log(e.text());
-                }
-            };
         $('#achieve li').hide();
-        achieves_selected.forEach(select);
+        achieves_selected
+        .map(select_subachieve)
+        .forEach(action_subview);
     };
 
 const on_scroll =
